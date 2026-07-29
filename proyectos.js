2560eced6827019cbb18fa1086727b1471ccf1c6
CarlosMoralesColombia <aguilabirra@gmail.com>
2026-07-28 19:27:00 -0500

    Proyectos realizados CM en el tiempo vinculados CV Web

---
+++ proyectos.js
@@ -0,0 +1,22 @@
+// Vinculo CM
+export const enlaceArchivoCV = 'Archivo_CV/Archivo_CV/CV_CarlosMorales.pdf'
+
+// Listado de proyectos:
+export const proyecto1 =
+{
+  nombre: 'Modelo FIRR.',
+  detalle: 'Es un modelo de datos asociado a la manera como fluyen los recursos desde su ingreso y su paso por todos los posibles estados hasta llegar a un resultado. Este modelo se genera de una combinación técnica y practica del concepto de "Organigrama Sideral" (circulación del dinero en las organizaciones) y la auditoría de cuentas, aterrizada a las entidades sin ánimo de lucro que buscan un retorno social.',
+  enlace: 'soportes_CV/Esquema_Modelo_FIRR.pdf'
+}
+export const proyecto2 =
+{
+  nombre: 'Proyectos BI.',
+  detalle: 'Proyectos de visualización de información mediante consultas a base de datos SQL en Power BI.',
+  enlace: 'https://app.powerbi.com/view?r=eyJrIjoiY2RjOTViZjktODM5Yy00ZWU4LWFmYTktYWJhNGYwNzI3NDk1IiwidCI6IjFjMjBkMDU2LWIzZTQtNGYwNy1hNTRjLTg0ZTQyMTZhMjkyMCIsImMiOjR9'
+}
+export const proyecto3 =
+{
+  nombre: 'Análisis Excel.',
+  detalle: 'Vinculación de archivos asociados a la suit Microsoft que perimiten tener la información disponible para análisis en el instante.',
+  enlace: 'soportes_CV/PPTO_OCCP_Cifras.pdf'
+}
