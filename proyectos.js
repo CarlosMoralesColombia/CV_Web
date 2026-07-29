7b1038ec9e606b89718283df3dc5fcb1bd42cb17
CarlosMoralesColombia <aguilabirra@gmail.com>
2026-07-28 21:17:08 -0500

    Actualización de CV y ajuste de dirección de los soportes

--- proyectos.js
+++ proyectos.js
@@ -6,7 +6,7 @@ export const proyecto1 =
 {
   nombre: 'Modelo FIRR.',
   detalle: 'Es un modelo de datos asociado a la manera como fluyen los recursos desde su ingreso y su paso por todos los posibles estados hasta llegar a un resultado. Este modelo se genera de una combinación técnica y practica del concepto de "Organigrama Sideral" (circulación del dinero en las organizaciones) y la auditoría de cuentas, aterrizada a las entidades sin ánimo de lucro que buscan un retorno social.',
-  enlace: 'soportes_CV/Esquema_Modelo_FIRR.pdf'
+  enlace: './soportes_CV/Esquema_Modelo_FIRR.pdf'
 }
 export const proyecto2 =
 {
@@ -18,5 +18,5 @@ export const proyecto3 =
 {
   nombre: 'Análisis Excel.',
   detalle: 'Vinculación de archivos asociados a la suit Microsoft que perimiten tener la información disponible para análisis en el instante.',
-  enlace: 'soportes_CV/PPTO_OCCP_Cifras.pdf'
+  enlace: './soportes_CV/PPTO_OCCP_Cifras.pdf'
 }
